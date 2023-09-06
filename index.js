989334b0727e82963ba138c81a646d198a9047c9
wizo06 <14183421+wizo06@users.noreply.github.com>
2023-09-06 00:31:36 -0400

    move elements into their own components

--- index.js
+++ index.js
@@ -12,88 +12,7 @@ Bun.serve({
     async fetch(req) { 
         const url = new URL(req.url);
         if (req.method === "GET" && url.pathname === "/") return new Response(Bun.file('base.html'));
-        if (req.method === "GET" && url.pathname === "/ingredients") return new Response(`
-        <form hx-post="/foods" hx-target="#insertAfterMe" hx-swap="beforeend">
-            <div class="grid">
-                <div>
-                    <label>Ingredients</label>
-                    <input list="ingredient" name="ingredient" required />
-                    <datalist id="ingredient">
-                        ${data.SRLegacyFoods.map(f => `<option value="${f.fdcId}">${f.description}</option>`)}
-                    </datalist>
-                </div>
-                <div>
-                    <label>Consumption amount (grams)</label>
-                    <input type="number" name="consumptionAmount" required />    
-                </div>
-                <div>
-                    <input id="daily" type="radio" name="consumptionType" value="daily" checked />
-                    <label for="daily">Daily</label>
-                    <br />
-                    <input id="weekly" type="radio" name="consumptionType" value="weekly" />
-                    <label for="weekly">Weekly</label>                
-                    <br />
-                    <input id="monthly" type="radio" name="consumptionType" value="monthly" />
-                    <label for="monthly">Monthly</label>
-                </div>    
-            </div>
-            <input type="submit" value="Add" />
-        </form>
-        <table>
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Ingredient</th>
-                    <th scope="col"><span data-tooltip="Protein (grams)">🥩</span></th>
-                    <th scope="col"><span data-tooltip="Fat (grams)">🥑</span></th>
-                    <th scope="col"><span data-tooltip="Carbs (grams)">🍠</span></th>
-                    <th scope="col"><span data-tooltip="Calories (kcal)">⚡</span></th>
-                    <th scope="col"><span data-tooltip="Price (USD)">💵</span></th>
-                    <th scope="col"><span data-tooltip="Daily Consumption (grams)">☀️</span></th>
-                    <th scope="col"><span data-tooltip="Weekly Consumption (grams)">7️⃣</span></th>
-                    <th scope="col"><span data-tooltip="Monthly Consumption (grams)">🌙</span></th>
-                </tr>
-            </thead>
-            <tbody id="insertAfterMe">
-                ${ingredients.map(i => `
-                <tr>
-                    <td scope="row">${i.fdcId}</td>
-                    <td>${i.description}</td>
-                    <td>${Math.round(i.foodNutrients.find(e => e.nutrient.id === 1003).amount * i.dailyConsumptionInGrams / SERVING_SIZE_IN_GRAMS_PER_NUTRIENT_AMOUNT)}</td>
-                    <td>${Math.round(i.foodNutrients.find(e => e.nutrient.id === 1004).amount * i.dailyConsumptionInGrams / SERVING_SIZE_IN_GRAMS_PER_NUTRIENT_AMOUNT)}</td>
-                    <td>${Math.round(i.foodNutrients.find(e => e.nutrient.id === 1005).amount * i.dailyConsumptionInGrams / SERVING_SIZE_IN_GRAMS_PER_NUTRIENT_AMOUNT)}</td>
-                    <td>${Math.round(i.foodNutrients.find(e => e.nutrient.id === 1008).amount * i.dailyConsumptionInGrams / SERVING_SIZE_IN_GRAMS_PER_NUTRIENT_AMOUNT)}</td>
-                    <td />
-                    <td>${i.dailyConsumptionInGrams}</td>
-                    <td>${i.dailyConsumptionInGrams * 7}</td>
-                    <td>${i.dailyConsumptionInGrams * 30}</td>
-                </tr>`)}
-            </tbody>
-            <tfoot>
-                <tr>
-                    <th scope="col">Total</td>
-                    <td />
-                    <td id="totalDailyProtein" />
-                    <td id="totalDailyFat" />
-                    <td id="totalDailyCarbs" />
-                    <td id="totalDailyCalories" />
-                    <td id="totalDailyPrice" />
-                    <td />
-                    <td />
-                    <td />
-                </tr>
-            </tfoot>
-        </table>
-        <input list="browsers" />
-        <datalist id="browsers">
-        <option value="Edge">
-        <option value="Firefox">
-        <option value="Chrome">
-        <option value="Opera">
-        <option value="Safari">
-        </datalist>
-        `);
-
+        if (req.method === "GET" && url.pathname === "/ingredients") return new Response(`${Form()}${Table()}`);
         if (req.method === "POST" && url.pathname === "/foods") {
             const formdata = await req.formData()
             const fdcId = formdata.get('ingredient')
@@ -135,26 +54,105 @@ Bun.serve({
                 .map(i => i.foodNutrients.find(e => e.nutrient.id === 1008).amount * i.dailyConsumptionInGrams / SERVING_SIZE_IN_GRAMS_PER_NUTRIENT_AMOUNT)
                 .reduce((acc, curr) => acc + curr)
             
-            return new Response(`
-            <tr>
-                <td scope="row">${food.fdcId}</td>
-                <td>${food.description}</td>
-                <td>${Math.round(food.foodNutrients.find(e => e.nutrient.id === 1003).amount * dailyConsumptionInGrams / SERVING_SIZE_IN_GRAMS_PER_NUTRIENT_AMOUNT)}</td>
-                <td>${Math.round(food.foodNutrients.find(e => e.nutrient.id === 1004).amount * dailyConsumptionInGrams / SERVING_SIZE_IN_GRAMS_PER_NUTRIENT_AMOUNT)}</td>
-                <td>${Math.round(food.foodNutrients.find(e => e.nutrient.id === 1005).amount * dailyConsumptionInGrams / SERVING_SIZE_IN_GRAMS_PER_NUTRIENT_AMOUNT)}</td>
-                <td>${Math.round(food.foodNutrients.find(e => e.nutrient.id === 1008).amount * dailyConsumptionInGrams / SERVING_SIZE_IN_GRAMS_PER_NUTRIENT_AMOUNT)}</td>
-                <td />
-                <td>${dailyConsumptionInGrams}</td>
-                <td>${dailyConsumptionInGrams * 7}</td>
-                <td>${dailyConsumptionInGrams * 30}</td>
-            </tr>
-            <td id="totalDailyProtein" hx-swap-oob="outerHTML">${Math.round(totalProtein)}</td>
-            <td id="totalDailyFat" hx-swap-oob="outerHTML">${Math.round(totalFat)}</td>
-            <td id="totalDailyCarbs" hx-swap-oob="outerHTML">${Math.round(totalCarbs)}</td>
-            <td id="totalDailyCalories" hx-swap-oob="outerHTML">${Math.round(totalCalories)}</td>
-            <td id="totalDailyPrice" hx-swap-oob="outerHTML">$$$</td>
-            `)
+            return new Response(`${NewRow(food, dailyConsumptionInGrams, totalProtein, totalFat, totalCarbs, totalCalories)}`)
         };
         return new Response(`<!DOCTYPE html><html>404 Not Found</html>`);
     }
 });
+
+const Form = () => `
+<form hx-post="/foods" hx-target="#insertAfterMe" hx-swap="beforeend">
+    <div class="grid">
+        <div>
+            <label>Ingredients</label>
+            <input list="ingredient" name="ingredient" required />
+            <datalist id="ingredient">
+                ${data.SRLegacyFoods.map(f => `<option value="${f.fdcId}">${f.description}</option>`)}
+            </datalist>
+        </div>
+        <div>
+            <label>Consumption amount (grams)</label>
+            <input type="number" name="consumptionAmount" required />    
+        </div>
+        <div>
+            <input id="daily" type="radio" name="consumptionType" value="daily" checked />
+            <label for="daily">Daily</label>
+            <br />
+            <input id="weekly" type="radio" name="consumptionType" value="weekly" />
+            <label for="weekly">Weekly</label>                
+            <br />
+            <input id="monthly" type="radio" name="consumptionType" value="monthly" />
+            <label for="monthly">Monthly</label>
+        </div>    
+    </div>
+    <input type="submit" value="Add" />
+</form>
+`
+
+const Table = () => `
+<table>
+    <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Ingredient</th>
+            <th scope="col"><span data-tooltip="Protein (grams)">🥩</span></th>
+            <th scope="col"><span data-tooltip="Fat (grams)">🥑</span></th>
+            <th scope="col"><span data-tooltip="Carbs (grams)">🍠</span></th>
+            <th scope="col"><span data-tooltip="Calories (kcal)">⚡</span></th>
+            <th scope="col"><span data-tooltip="Price (USD)">💵</span></th>
+            <th scope="col"><span data-tooltip="Daily Consumption (grams)">☀️</span></th>
+            <th scope="col"><span data-tooltip="Weekly Consumption (grams)">7️⃣</span></th>
+            <th scope="col"><span data-tooltip="Monthly Consumption (grams)">🌙</span></th>
+        </tr>
+    </thead>
+    <tbody id="insertAfterMe">
+        ${ingredients.map(i => `
+        <tr>
+            <td scope="row">${i.fdcId}</td>
+            <td>${i.description}</td>
+            <td>${Math.round(i.foodNutrients.find(e => e.nutrient.id === 1003).amount * i.dailyConsumptionInGrams / SERVING_SIZE_IN_GRAMS_PER_NUTRIENT_AMOUNT)}</td>
+            <td>${Math.round(i.foodNutrients.find(e => e.nutrient.id === 1004).amount * i.dailyConsumptionInGrams / SERVING_SIZE_IN_GRAMS_PER_NUTRIENT_AMOUNT)}</td>
+            <td>${Math.round(i.foodNutrients.find(e => e.nutrient.id === 1005).amount * i.dailyConsumptionInGrams / SERVING_SIZE_IN_GRAMS_PER_NUTRIENT_AMOUNT)}</td>
+            <td>${Math.round(i.foodNutrients.find(e => e.nutrient.id === 1008).amount * i.dailyConsumptionInGrams / SERVING_SIZE_IN_GRAMS_PER_NUTRIENT_AMOUNT)}</td>
+            <td />
+            <td>${i.dailyConsumptionInGrams}</td>
+            <td>${i.dailyConsumptionInGrams * 7}</td>
+            <td>${i.dailyConsumptionInGrams * 30}</td>
+        </tr>`)}
+    </tbody>
+    <tfoot>
+        <tr>
+            <th scope="col">Total</td>
+            <td />
+            <td id="totalDailyProtein" />
+            <td id="totalDailyFat" />
+            <td id="totalDailyCarbs" />
+            <td id="totalDailyCalories" />
+            <td id="totalDailyPrice" />
+            <td />
+            <td />
+            <td />
+        </tr>
+    </tfoot>
+</table>
+`
+
+const NewRow = (food, dailyConsumptionInGrams, totalProtein, totalFat, totalCarbs, totalCalories) => `
+<tr>
+    <td scope="row">${food.fdcId}</td>
+    <td>${food.description}</td>
+    <td>${Math.round(food.foodNutrients.find(e => e.nutrient.id === 1003).amount * dailyConsumptionInGrams / SERVING_SIZE_IN_GRAMS_PER_NUTRIENT_AMOUNT)}</td>
+    <td>${Math.round(food.foodNutrients.find(e => e.nutrient.id === 1004).amount * dailyConsumptionInGrams / SERVING_SIZE_IN_GRAMS_PER_NUTRIENT_AMOUNT)}</td>
+    <td>${Math.round(food.foodNutrients.find(e => e.nutrient.id === 1005).amount * dailyConsumptionInGrams / SERVING_SIZE_IN_GRAMS_PER_NUTRIENT_AMOUNT)}</td>
+    <td>${Math.round(food.foodNutrients.find(e => e.nutrient.id === 1008).amount * dailyConsumptionInGrams / SERVING_SIZE_IN_GRAMS_PER_NUTRIENT_AMOUNT)}</td>
+    <td />
+    <td>${dailyConsumptionInGrams}</td>
+    <td>${dailyConsumptionInGrams * 7}</td>
+    <td>${dailyConsumptionInGrams * 30}</td>
+</tr>
+<td id="totalDailyProtein" hx-swap-oob="outerHTML">${Math.round(totalProtein)}</td>
+<td id="totalDailyFat" hx-swap-oob="outerHTML">${Math.round(totalFat)}</td>
+<td id="totalDailyCarbs" hx-swap-oob="outerHTML">${Math.round(totalCarbs)}</td>
+<td id="totalDailyCalories" hx-swap-oob="outerHTML">${Math.round(totalCalories)}</td>
+<td id="totalDailyPrice" hx-swap-oob="outerHTML">$$$</td>
+`
